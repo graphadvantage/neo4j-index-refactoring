0f0bb84baf00adc4fc2550a197b6c348bb4adcf1
Michael Moore <v-michmo@microsoft.com>
2016-09-19 08:25:22 -0700

    edits

--- batch-index-refactor.py
+++ batch-index-refactor.py
@@ -1,4 +1,5 @@
-# STEP 5: Refactor Children to Parent Category, in batches by indexes on both to restrict cartesian
+# STEP 5: Refactor Children to Parent Category, in batches by indexes on both
+# Bolt variant: using session, implicit transactions
 
 import time
 
@@ -10,30 +11,41 @@ driver = GraphDatabase.driver("bolt://localhost",
                               trust=TRUST_ON_FIRST_USE)
 session = driver.session()
 
+# get counts of child nodes by property that needs refactoring
 childProps1 = '''
 MATCH (n:Organization)
 WHERE NOT ((n)-[:HAS_LOCATION]-())
 RETURN n.country AS pname, count(n) AS ncount
 '''
 
+# refactor, passing in the property value to both parent and child as paramters to restrict cartesian
 indexRefactor1 = '''
 MATCH (c:Country {countryName:{pname}}), (n:Organization {country:{pname}})
 WHERE NOT ((n)-[:HAS_LOCATION]-())
 WITH c,n LIMIT {limit} MERGE (c)<-[r:HAS_LOCATION]-(n)
 '''
+
 ntotal = 0
+
+# configure batch size
 batchSize = 2000
 
 
 try:
     tjob = time.time()
+
+    #get category and child node counts
     print("processing ---> getting child property list for refactoring")
 
     propertyList = session.run(childProps1)
 
     print('-----------------')
+
+    # refactor, stepping through category values in batches per child node counts
     print("processing ---> starting refactoring to parent category")
+
     print ("%s %s" % ("using batch size: ", batchSize))
+
     print('-----------------')
 
     for property in propertyList:
@@ -44,15 +56,18 @@ try:
         ncount = (property["ncount"])
 
         while True:
+            # refactor, passing in the property value to match both parent and child on indexes
             result = session.run(indexRefactor1, {"pname": pname, "limit": batchSize})
             btotal = btotal + batchSize
             summary = result.consume()
             counters = summary.counters
             print(counters)
 
+            # compare running total of batched nodes to total nodes
             if btotal < ncount:
                 print('--next batch--')
 
+            # end batches, go to next parent category value
             else:
                 print('-----done-----')
                 break
